713313a71b0b072a71572533d575508b782250df
Ali <taherkhany.dev@gmail.com>
2023-09-11 10:04:53 +0330

    Added Some Files

--- manipulating-files/file-stats.js
+++ manipulating-files/file-stats.js
@@ -18,23 +18,23 @@ fs.stat('./data/users.txt', (err, stats) => {
 
 // Sync Method
 
-try {
-    const stats = fs.statSync('./data/users.txt')
-    console.log('Sync Method')
-    console.log(stats.size)
-} catch (err) {
-    console.log(err)
-} // this method blocks the thread until the file stats are ready
+// try {
+//     const stats = fs.statSync('./data/users.txt')
+//     console.log('Sync Method')
+//     console.log(stats.size)
+// } catch (err) {
+//     console.log(err)
+// } // this method blocks the thread until the file stats are ready
 
 // Promise Based Method
 
-async function sample() {
-    try {
-        const stats = await promiseFs.stat('./data/users.txt')
-        console.log('Promise Based Method')
-        console.log(stats.isFile())
-    } catch (err) {
-        console.log(err)
-    }
-}
-sample()
+// async function sample() {
+//     try {
+//         const stats = await promiseFs.stat('./data/users.txt')
+//         console.log('Promise Based Method')
+//         console.log(stats.isFile())
+//     } catch (err) {
+//         console.log(err)
+//     }
+// }
+// sample()
